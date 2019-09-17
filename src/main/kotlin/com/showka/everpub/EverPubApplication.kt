06708f424cf62183e1488801fab8c37881fef6ea
John Doe <johndoe@example.com>
2019-09-17 20:17:42 +0900

    format & rename package

--- src/main/kotlin/com/showka/everpub/EverPubApplication.kt
+++ src/main/kotlin/com/showka/everpub/EverPubApplication.kt
@@ -1,4 +1,4 @@
-package com.example.everpub
+package com.showka.everpub
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
@@ -7,5 +7,5 @@ import org.springframework.boot.runApplication
 class EverPubApplication
 
 fun main(args: Array<String>) {
-	runApplication<EverPubApplication>(*args)
+    runApplication<EverPubApplication>(*args)
 }
--- src/test/kotlin/com/showka/everpub/EverPubApplicationTests.kt
+++ src/test/kotlin/com/showka/everpub/EverPubApplicationTests.kt
@@ -1,4 +1,4 @@
-package com.example.everpub
+package com.showka.everpub
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest
 @SpringBootTest
 class EverPubApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
+    @Test
+    fun contextLoads() {
+    }
 
 }
