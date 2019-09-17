3941c07f29cea1351818056d57ad87789fbc0f33
John Doe <johndoe@example.com>
2019-09-17 17:37:14 +0900

    commit initial resource

---
+++ src/test/kotlin/com/example/everpub/EverPubApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.everpub
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EverPubApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
