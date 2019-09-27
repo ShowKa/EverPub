0e8d4730b09b9093deb6eefd14b3840c376f672e
John Doe <johndoe@example.com>
2019-09-27 22:44:20 +0900

    bug fix

--- src/main/kotlin/com/showka/everpub/publish/epub/PublisherEpub.kt
+++ src/main/kotlin/com/showka/everpub/publish/epub/PublisherEpub.kt
@@ -17,10 +17,10 @@ class PublisherEpub(private val chapter: Chapter) : Publisher {
 
     override fun publish(): String {
         val header = """
-            <?xml version="1.0" encoding="utf-8" standalone="no"?>
-            <!DOCTYPE html>
-            <html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops" lang="ja" xml:lang="ja">
-        """
+            |<?xml version="1.0" encoding="utf-8" standalone="no"?>
+            |<!DOCTYPE html>
+            |<html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops" lang="ja" xml:lang="ja">
+        """.trimMargin()
         // <title /><body> chapter </body>
         val publisher = PublisherChapter(this.chapter)
         publisher.setGrade(this.grade)
