a8dd57378328ceeb8c16bc0e6642b39d10cd09eb
John Doe <johndoe@example.com>
2019-10-04 22:00:54 +0900

    User Dictionary

--- src/main/kotlin/com/showka/everpub/novelmarkup/NmTokens.kt
+++ src/main/kotlin/com/showka/everpub/novelmarkup/NmTokens.kt
@@ -2,6 +2,8 @@ package com.showka.everpub.novelmarkup
 
 import com.atilika.kuromoji.TokenizerBase
 import com.atilika.kuromoji.ipadic.Tokenizer
+import org.springframework.core.io.ClassPathResource
+import java.io.InputStream
 
 class NmTokens(text: String) : ArrayList<NmToken>() {
 
@@ -14,4 +16,7 @@ class NmTokens(text: String) : ArrayList<NmToken>() {
     }
 }
 
-private val tokenizer = Tokenizer.Builder().mode(TokenizerBase.Mode.NORMAL).build()
+private const val pathOfDictionary = "/kuromoji/dictionary.csv"
+private val dic: InputStream = ClassPathResource(pathOfDictionary).file.inputStream()
+private val tokenizer = Tokenizer.Builder().mode(TokenizerBase.Mode.NORMAL)
+        .userDictionary(dic).build<Tokenizer>()
